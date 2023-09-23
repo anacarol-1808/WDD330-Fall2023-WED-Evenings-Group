ce7544938ff1c7e0616ef946846ce049c739e7cb
anacarol-1808 <anacarol_1808@hotmail.com>
2023-09-23 14:18:56 -0600

    as Matthew I modified product.js to store cart ad and array istead of single object

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,14 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import { findProductById } from './productData.mjs';
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cartItems = getLocalStorage('so-cart');
+  if (cartItems) {
+    cartItems.push(product);
+  } else {
+    cartItems = [product]
+  }
+  setLocalStorage('so-cart', cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
