4c305aa386a7670b964e128f53fa6d323c450ca5
anacarol-1808 <anacarol_1808@hotmail.com>
2023-10-10 00:09:50 -0600

    css for cart number of items created, but needs adjustments

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
-import  productList from "./productList.mjs"
-const category = "tents"
-const selector = "productList"
+import productList from "./productList.mjs";
+const category = "tents";
+const selector = "productList";
 productList(category, selector);
-
