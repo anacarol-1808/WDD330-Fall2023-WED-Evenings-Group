cfd9c43bf69f7343e5eb421af4e905889aa92c37
anacarol-1808 <anacarol_1808@hotmail.com>
2023-10-10 22:32:05 -0600

    cart icon displays amount inside cart

--- src/js/product.js
+++ src/js/product.js
@@ -7,3 +7,21 @@ productDetails(productId);
 // Testing getParams and findProductById
 // console.log(getParam("product"))
 // console.log(findProductById(getParam("product")))
+
+
+let cartCount = 0;
+
+const addToCartBtn = document.getElementById('addToCart');
+const cartBadge = document.getElementById('cart-count');
+
+addToCartBtn.addEventListener('click', (event) => {
+    cartCount++;
+
+    setTimeout((e) => {
+        cartBadge.innerHTML = cartCount;
+    }, 500);
+
+    
+})
+
+
