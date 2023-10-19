7a3829b31c63a96aade310aedbb3a215fe9c5ecb
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2023-10-19 13:51:58 -0700

    Updated HTML Headers on Product List

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,7 +1,7 @@
-import { getData } from "./productData.mjs"
+import { getData } from "./productData.mjs";
 
-function productCardTemplate (product, category) {
-    return `<li class="product-card">
+function productCardTemplate(product, category) {
+  return `<li class="product-card">
     <a href="/product_pages/index.html?category=${category}&product=${product.Id}">
       <img
         src="${product.Images.PrimaryMedium}"
@@ -10,19 +10,38 @@ function productCardTemplate (product, category) {
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p></a>
-  </li>`
-};
+  </li>`;
+}
 
 export default async function productList(category, selector) {
-    const list = await getData(category);
-    const output = document.getElementById(selector);
-    if (output) {
-      renderList(list["Result"], output, category);
-    }
-};
+  const list = await getData(category);
+  const output = document.getElementById(selector);
+  if (output) {
+    renderList(list["Result"], output, category);
+  }
+}
 
 function renderList(list, output, category) {
-    list.map((product)=> {
-        output.insertAdjacentHTML("beforeend", productCardTemplate(product, category))  
-    })
+  formatHeader(category)
+  list.map((product) => {
+    output.insertAdjacentHTML(
+      "beforeend",
+      productCardTemplate(product, category)
+    );
+  });
+}
+
+function formatHeader(category) {
+  // Replace hyphens in category with spaces
+  let categoryHeader = category.replace(/-/g, " ");
+  // Capitalize each word
+  categoryHeader = categoryHeader
+    .toLowerCase()
+    .split(" ")
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(" ");
+  // Add to HTML
+  document.querySelector(
+      ".product-list-header"
+    ).textContent = `Top Products: ${categoryHeader}`;
 }
