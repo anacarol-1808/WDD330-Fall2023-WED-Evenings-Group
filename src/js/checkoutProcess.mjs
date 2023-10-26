5df9c739b94b123ed835fba8c8db4d2bcbdace48
Daffadelicious <31669829+Daffadelicious@users.noreply.github.com>
2023-10-25 18:48:08 -0700

    start team 07

---
+++ src/js/checkoutProcess.mjs
@@ -0,0 +1,23 @@
+// Sales tax = 6%
+// Shipping = $10 + $2 per additional item
+// http://server-nodejs.cit.byui.edu:3000/checkout
+
+export default checkoutProcess = {
+    localKey: "wip",
+    outputSelector: "wip",
+    currentTotal: 0,
+    tax: 0,
+    shipping: 0,
+    init: function(localKey, outputSelector){
+
+    },
+    calculateItemTotal: function(){
+
+    },
+    calculateFinalPrice: function(){
+
+    },
+    displayFinalPrice: function(){
+
+    }   
+}
